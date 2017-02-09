9207f5de64eb3ac497c954740298648b38301122
Jaden Wang <jadenyjw@gmail.com>
2017-02-09 17:59:03 -0500

    Data preprocessing
    Former-commit-id: a1bedaa35cc9729badaddebe4a0be054e986c9e8

--- ml/train.py
+++ ml/train.py
@@ -3,15 +3,19 @@ import numpy as np
 
 from matplotlib import pyplot as plt
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Dense, Dropout, Activation, Flatten, Reshape
 from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D, Convolution1D
 from keras.utils import np_utils
 from keras.utils.io_utils import HDF5Matrix
 from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import SGD
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import TensorBoard
+
 batch_size = 135
 nb_classes = 250
-nb_epoch = 100
+nb_epoch = 600
 
 # input image dimensions
 img_rows, img_cols = 225, 225
@@ -21,10 +25,6 @@ Y_train = np.asarray(HDF5Matrix('training.h5', 'Y'))
 
 X_test = np.asarray(HDF5Matrix('test.h5', 'X'))
 Y_test = np.asarray(HDF5Matrix('test.h5', 'Y'))
-
-# number of convolutional filters to use
-# the data, shuffled and split between train and test sets
-#(X_train, y_train), (X_test, y_test) = mnist.load_data()
 
 if K.image_dim_ordering() == 'th':
     X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
@@ -42,10 +42,17 @@ print('X_train shape:', X_train.shape)
 print(X_train.shape[0], 'train samples')
 print(X_test.shape[0], 'test samples')
 
-datagen = ImageDataGenerator()
+datagen = ImageDataGenerator(featurewise_center=True,
+    featurewise_std_normalization=True,
+    rotation_range=5,
+    width_shift_range=0.14,
+    height_shift_range=0.14,
+    horizontal_flip=True)
+datagen_test = ImageDataGenerator(featurewise_center=True,
+    featurewise_std_normalization=True)
+
 datagen.fit(X_train)
-
-
+datagen_test.fit(X_test)
 # convert class vectors to binary class matrices
 
 #Y_train = np_utils.to_categorical(y_train, nb_classes)
@@ -53,60 +60,72 @@ datagen.fit(X_train)
 
 model = Sequential()
 
-model.add(Convolution2D(64, 15, 15,
+#L1
+model.add(Convolution2D(nb_filter=64, nb_row=15, nb_col=15,
                         border_mode='valid',
-                        input_shape=[225, 225, 1],
+                        input_shape=input_shape,
                         activation='relu',
                         subsample=(3,3)))
-#model.add(ZeroPadding2D(padding=(0,0), dim_ordering='default'))
+#model.add(ZeroPadding2D(padding=(0,0)))
 model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
 
-model.add(Convolution2D(128, 5, 5,
+#L2
+model.add(Convolution2D(nb_filter=128, nb_row=5, nb_col=5,
                         activation='relu',
                         subsample=(1,1)))
-model.add(ZeroPadding2D(padding=(0,0), dim_ordering='default'))
+#model.add(ZeroPadding2D(padding=(0,0)))
 model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
 
+#L3
+model.add(Convolution2D(nb_filter=256, nb_row=3, nb_col=3,
+                        activation='relu', border_mode='same',
+                        subsample=(1,1)))
+#model.add(ZeroPadding2D(padding=(1,1)))
 
-model.add(Convolution2D(256, 3, 3,
-                        activation='relu',
+#L4
+model.add(Convolution2D(nb_filter=256, nb_row=3, nb_col=3,
+                        activation='relu', border_mode='same',
                         subsample=(1,1)))
-model.add(ZeroPadding2D(padding=(1,1), dim_ordering='default'))
-model.add(Convolution2D(256, 3, 3,
-                        activation='relu',
+#model.add(ZeroPadding2D(padding=(1,1)))
+
+#L5
+model.add(Convolution2D(nb_filter=256, nb_row=3, nb_col=3,
+                        activation='relu', border_mode='same',
                         subsample=(1,1)))
-model.add(ZeroPadding2D(padding=(1,1), dim_ordering='default'))
-model.add(Convolution2D(256, 3, 3,
-                        activation='relu',
-                        subsample=(1,1)))
-model.add(ZeroPadding2D(padding=(1,1), dim_ordering='default'))
+#model.add(ZeroPadding2D(padding=(1,1)))
+
 model.add(MaxPooling2D(pool_size=(3,3), strides=(2,2)))
-
-model.add(Convolution2D(512, 7, 7,
+#L6
+model.add(Convolution2D(nb_filter=512, nb_row=7, nb_col=7,
                         activation='relu',
                         subsample=(1,1)))
-model.add(ZeroPadding2D(padding=(0,0), dim_ordering='default'))
+#model.add(ZeroPadding2D(padding=(0,0)))
+
+model.add(Dropout(0.5))
+#L7
+model.add(Convolution2D(nb_filter=512, nb_row=1, nb_col=1,
+                        activation='relu',
+                        subsample=(1,1)))
+#model.add(ZeroPadding2D(padding=(0,0)))
 
 model.add(Dropout(0.5))
 
-model.add(Convolution2D(512, 1, 1,
+model.add(Convolution2D(nb_filter=250, nb_row=1, nb_col=1,
                         activation='relu',
                         subsample=(1,1)))
-model.add(ZeroPadding2D(padding=(0,0), dim_ordering='default'))
-
-model.add(Dropout(0.5))
-
-model.add(Convolution2D(250, 1, 1, subsample=(1,1)))
+#model.add(ZeroPadding2D(padding=(0,0)))
+model.add(Flatten())
 model.add(Activation("softmax"))
-
+sgd = SGD(lr=0.003, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
+              optimizer=sgd,
               metrics=['accuracy'])
-
-model.fit_generator(datagen.flow(X_train, Y_train, batch_size=135), nb_epoch=nb_epoch,
-          verbose=1, validation_data=(X_test, Y_test))
+checkpointer = ModelCheckpoint(filepath="checkpoint.hdf5", verbose=1, save_best_only=True)
+tensorboard = TensorBoard(log_dir='logs', histogram_freq=0, write_graph=True, write_images=False)
+model.fit_generator(datagen.flow(X_train, Y_train, batch_size=135), samples_per_epoch=18000, nb_epoch=nb_epoch,
+          verbose=1, validation_data=datagen_test.flow(X_test, Y_test), callbacks=[checkpointer, tensorboard], nb_val_samples=2000)
 score = model.evaluate(X_test, Y_test, verbose=0)
-model.save('my_model.h5')
+model.save('drawpvp.h5')
 
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
