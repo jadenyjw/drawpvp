fd6347f55e0552f87a67f9e0f868da3a8093e426
Jaden Wang <jadenyjw@gmail.com>
2017-02-08 20:02:54 -0500

    Experiment with SGD and DL4J library

---
+++ src/main/java/ai/NeuralNet.java
@@ -0,0 +1,14 @@
+package ai;
+
+import java.io.IOException;
+
+import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
+import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+
+public class NeuralNet {
+	public static void main (String[] args) throws IOException, InvalidKerasConfigurationException, UnsupportedKerasConfigurationException{
+		MultiLayerNetwork network = KerasModelImport.importKerasSequentialModelAndWeights("ml/checkpoint.hdf5");
+	}
+}
