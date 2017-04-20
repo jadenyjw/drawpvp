7ccfbcb622c4c9b9d1505c3b287df7a0cb93794c
Jaden Wang <jadenyjw@gmail.com>
2017-04-20 14:14:00 -0400

    Give up writing own networking code and resort to Kryonet library

--- src/main/java/tests/Tests.java
+++ src/main/java/tests/Tests.java
@@ -10,43 +10,10 @@ import java.net.InetAddress;
 public class Tests {
 
     public static void main(String[] args) throws Exception{
-        testServer();
-        testClient();
+
         //testNeuralNet();
     }
 
-    private static void testServer(){
-        try {
-            Server server = new Server();
-            server.start();
-        }
-        catch (Exception e){
-            System.out.println(e);
-        }
-    }
-
-    private static void testClient(){
-        try {
-            Client client = new Client(InetAddress.getLocalHost().getHostAddress());
-            client.start();
-            Client client2 = new Client(InetAddress.getLocalHost().getHostAddress());
-            client2.start();
-            try {
-                Thread.sleep(1000);                 //1000 milliseconds is one second.
-            } catch(InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-            for(long x = 0; x < 100; x++){
-
-                client.sendMessage("Hello");
-                client2.sendMessage("h2");
-            }
-
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-    }
 
     private static void testNeuralNet(){
         try {
