1ddf65eb24c4bb2a8bcd3cbdc7390e50e281b1e1
Jaden Wang <jadenyjw@gmail.com>
2017-05-09 10:48:17 -0400

    Figured out how to switch scenes with JFXButton

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -2,6 +2,7 @@ package gui;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -11,29 +12,22 @@ import java.io.IOException;
 public class Main extends Application {
     private Stage primaryStage;
     private AnchorPane mainLayout;
+    private AnchorPane lobbyLayout;
 
     public void start(Stage primaryStage) throws IOException{
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("dRAWPVP");
-        showMain_menu();
+        showMainMenu();
     }
-    private void showMain_menu() throws IOException
+    public void showMainMenu() throws IOException
     {
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("/gui/main_menu.fxml"));
+        loader.setLocation(Main.class.getResource("/views/main_menu.fxml"));
         mainLayout = loader.load();
         Scene scene = new Scene(mainLayout);
         primaryStage.setScene(scene);
         primaryStage.show();
 
-    }
-
-    public static void showElectricalScene() throws IOException
-    {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("/gui/lobby.fxml"));
-        AnchorPane lobbyPane = loader.load();
-        Scene scene = new Scene(lobbyPane);
     }
 
     public static void main(String[] args)
--- src/main/java/gui/MenuController.java
+++ src/main/java/gui/MenuController.java
@@ -1,20 +1,29 @@
 package gui;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import gui.Main;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * Created by jaden on 4/29/17.
- */
-public class MenuController
-{
-    private Main main;
-  @FXML
 
-    private void goElectrical() throws IOException
-  {
-    Main.showElectricalScene();
+
+public class MenuController {
+
+  @FXML
+  private JFXButton newGameButton;
+
+  @FXML
+  protected void goElectrical(ActionEvent e) throws IOException{
+    Stage stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+    // these two of them return the same stage
+    // Swap screen
+    Parent pane = FXMLLoader.load(Main.class.getResource("/views/lobby.fxml"));
+    stage.getScene().setRoot(pane);
   }
 }
