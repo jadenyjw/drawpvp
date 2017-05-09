966bffc9cdeb462a3cb10ef14e98c3b51bd453e9
Jaden Wang <jadenyjw@gmail.com>
2017-05-09 11:01:14 -0400

    Put in networking methods to new game button

--- src/main/java/gui/MenuController.java
+++ src/main/java/gui/MenuController.java
@@ -8,9 +8,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
+import networking.DClient;
+import networking.DServer;
 
 import java.io.IOException;
-
+import java.net.InetAddress;
 
 
 public class MenuController {
@@ -19,11 +21,12 @@ public class MenuController {
   private JFXButton newGameButton;
 
   @FXML
-  protected void goElectrical(ActionEvent e) throws IOException{
+  protected void hostLobbyScreen(ActionEvent e) throws IOException{
     Stage stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
-    // these two of them return the same stage
-    // Swap screen
     Parent pane = FXMLLoader.load(Main.class.getResource("/views/lobby.fxml"));
     stage.getScene().setRoot(pane);
+    DServer server = new DServer();
+    DClient client = new DClient(InetAddress.getLocalHost().getHostAddress(), "Ur mum");
+    client.joinGame();
   }
 }
