99e3ffbfe4d0bab3ed86ee25650bc5d7af45dca8
ll <ll>
2017-05-09 07:36:42 -0400

    Tinkering with controller

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -1,24 +1,43 @@
 package gui;
 
-import ai.NeuralNet;
-import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.IOException;
 
-    public void start(Stage primaryStage) throws Exception
+public class Main extends Application {
+    private Stage primaryStage;
+    private AnchorPane mainLayout;
+
+    public void start(Stage primaryStage) throws IOException{
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("dRAWPVP");
+        showMain_menu();
+    }
+    private void showMain_menu() throws IOException
     {
-        Parent root = FXMLLoader.load(getClass().getResource("/views/main_menu.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/gui/main_menu.fxml"));
+        mainLayout = loader.load();
+        Scene scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
         primaryStage.show();
 
     }
-    public static void main(String[] args) {
+
+    public static void showElectricalScene() throws IOException
+    {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("/gui/lobby.fxml"));
+        AnchorPane lobbyPane = loader.load();
+        Scene scene = new Scene(lobbyPane);
+    }
+
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
--- src/main/java/gui/MenuController.java
+++ src/main/java/gui/MenuController.java
@@ -1,7 +1,20 @@
 package gui;
 
+import javafx.fxml.FXML;
+import gui.Main;
+
+import java.io.IOException;
+
 /**
  * Created by jaden on 4/29/17.
  */
-public class MenuController {
+public class MenuController
+{
+    private Main main;
+  @FXML
+
+    private void goElectrical() throws IOException
+  {
+    Main.showElectricalScene();
+  }
 }
