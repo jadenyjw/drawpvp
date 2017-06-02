56b0c0551072a41d5e8f9036f0cae65135e7c861
Zhong <Akeen.Zhong@student.tdsb.on.ca>
2017-06-02 13:47:12 -0400

    Lobby screen looks nice

--- src/main/java/gui/GameController.java
+++ src/main/java/gui/GameController.java
@@ -1,9 +1,20 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
 /**
  * Created by jaden on 5/10/17.
  */
 public class GameController {
+
+    @FXML protected Canvas surface;
+    GraphicsContext gc = surface.getGraphicsContext2D();
+
+    private void draw(GraphicsContext gc){
+        gc.fillRect(50,60,70,80);
+    }
 
 
 }
